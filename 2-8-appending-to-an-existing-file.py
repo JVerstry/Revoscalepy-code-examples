e3cf5eee36fabae4d1e796aa0c7dcf0c6b578d9b
Jérôme Verstrynge <tshikatshikaaa@gmail.com>
2018-07-04 07:30:05 +0200

    Example 2.8

---
+++ 2-8-appending-to-an-existing-file.py
@@ -0,0 +1,28 @@
+# 2.8 Append to an existing file
+import os
+import settings as st
+from revoscalepy import rx_import, rx_get_info, RxOptions
+
+# Input file
+firstFile = os.path.join(st.SAMPLE_DATA_DIR,'claims.txt')
+
+# Output file
+outfile = os.path.join(st.RESULTS_LOCATION,'claimsclaims.xdf')
+
+# Creating 1st file
+rx_import(input_data = firstFile, output_file = outfile,
+    strings_as_factors = True,
+    overwrite = True)
+
+# Displaying info
+claims_data_frame = rx_import(outfile)
+print(rx_get_info(claims_data_frame, get_var_info = True))
+
+# Appending the same file to the output
+rx_import(input_data = firstFile, output_file = outfile,
+    strings_as_factors = True,
+    append = "rows")
+
+# Displaying info
+claims_data_frame = rx_import(outfile)
+print(rx_get_info(claims_data_frame, get_var_info = True))

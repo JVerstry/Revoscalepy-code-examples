0202f5c121d37ae5d6dc7ddfab9efc7c748eb4b3
Jérôme Verstrynge <tshikatshikaaa@gmail.com>
2018-07-04 08:01:33 +0200

    Example 2.9

---
+++ 2-9-transforming-data-on-import.py
@@ -0,0 +1,7 @@
+# 2.9 Transforming Data on Import
+
+## The 'transforms' parameter of rx_import is currently not supported
+## See https://docs.microsoft.com/en-us/machine-learning-server/python-reference/revoscalepy/rx-import#transforms
+
+## The 'transform_function' parameter can be explored as a workaround
+## See https://docs.microsoft.com/en-us/machine-learning-server/python-reference/revoscalepy/rx-import#transform_function

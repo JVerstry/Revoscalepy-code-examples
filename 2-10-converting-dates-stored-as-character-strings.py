c4cdc41840337d83626735c56d49ec69fd5198bb
Jérôme Verstrynge <tshikatshikaaa@gmail.com>
2018-07-04 08:07:06 +0200

    Example 2.10

---
+++ 2-10-converting-dates-stored-as-character-strings.py
@@ -0,0 +1,7 @@
+# 2.10 Converting Dates Stored As Character Strings
+
+## The 'transforms' parameter of rx_import is currently not supported
+## See https://docs.microsoft.com/en-us/machine-learning-server/python-reference/revoscalepy/rx-import#transforms
+
+## The 'transform_function' parameter can be explored as a workaround
+## See https://docs.microsoft.com/en-us/machine-learning-server/python-reference/revoscalepy/rx-import#transform_function

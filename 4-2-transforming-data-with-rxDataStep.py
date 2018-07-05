25f0f198e3b220471236047a20a53c44ba038ab4
Jérôme Verstrynge <tshikatshikaaa@gmail.com>
2018-07-05 17:01:12 +0200

    Example 4.2

---
+++ 4-2-transforming-data-with-rxDataStep.py
@@ -0,0 +1,4 @@
+# 4.2 Transforming Data with rxDataStep
+
+## REM: the 'transforms' parameter of rx_data_step is not implemented
+## in RevoscalePy. Workarounds can be implemented in standard Python.
